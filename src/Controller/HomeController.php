5504cac586715d7b5fc8c8e4611caf152f04e243
JohnDario <35594560+JohnDario@users.noreply.github.com>
2018-03-27 17:42:58 +0200

    test

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,19 +2,24 @@
 
 namespace App\Controller;
 
-use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends Controller
 {
     /**
      * @Route("/home", name="home")
      */
-    public function index()
+    public function index(UserRepository $userRepo)
     {
-        $message = 'Bonjour à tous';
+        // $userRepo est passé automatiquement en parametre par Symfony
+        // -> injection de dépendance. On n'a donc pas a l'instancier nous-meme
+        // $userRepo effectuera ici un SELECT * FROM user ...
+        $userlist = $userRepo->findAll();
+        
         return $this->render("home.html.twig",[
-            'msg' => $message
+            'users' => $userlist
         ]);
     }
 }
